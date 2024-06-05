195041c16ed5f40826e8643f14cd11947aec5973
Kartik Rane <kartiksrane05@gmail.com>
2024-06-05 14:34:03 +0530

    using library bits

--- Readme.md
+++ Readme.md
@@ -1,5 +1,7 @@
 # Getting Started
 ##### Basic I/O
+     - iostream library
+     - bits/stdc++.h 
 ##### Data Types
 
     
---
+++ usingbitslibrary.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int x = 10;
+  int y;
+  cin>>y;
+  int sum = x + y;
+  cout<<"The value of sum is "<<sum;
+  return 0;
+}
