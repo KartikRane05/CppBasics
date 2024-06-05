b3c5f4d3c5db653c41ba4fb7f89588d6e2c20d11
Kartik Rane <kartiksrane05@gmail.com>
2024-06-05 14:23:14 +0530

    Basic I/O complete

---
+++ Cin.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x,y;
+  cin >> x>>y;
+  cout << "The value of x is " << x<<" and y is "<<y;
+  return 0;
+}
---
+++ Cout.cpp
@@ -0,0 +1,8 @@
+// this is starting with hello world program
+#include <iostream>
+using namespace std;
+int main() { 
+    cout<<"Kartik"<<"\n";
+    cout<<"Kartik"<<"\n";
+  return 0;
+  }
